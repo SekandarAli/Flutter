77026d7f69164cd028ff6df38cfc6dc49d33b428
Sekandar <isekandarali@gmail.com>
2021-09-30 22:45:11 +0500

    Day 12 Done

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,12 +1,32 @@
 // ignore_for_file: prefer_const_constructors, camel_case_types
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widgets.dart';
 
-class Home_page extends StatelessWidget {
+class Home_page extends StatefulWidget {
   const Home_page({ Key? key }) : super(key: key);
+
+  @override
+  State<Home_page> createState() => _Home_pageState();
+}
+
+class _Home_pageState extends State<Home_page> {
+
+  @override
+  void initState() {
+    super.initState();
+    //loadData();
+  }
+
+  // loadData() async{
+  //  var CatalogJson = await rootBundle.loadString("assets/files/catalog.json");
+  //  var JsonDecode = jsonEncode(CatalogModel);
+  // }
 
   @override
   Widget build(BuildContext context) {
